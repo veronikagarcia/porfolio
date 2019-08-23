aa4da55c0cf2ea1e43dfb8115731a13271ae690d
veronikagarcia <49323509+veronikagarcia@users.noreply.github.com>
2019-08-23 19:47:47 -0300

    Udpate!

--- js/vegarciajs.js
+++ js/vegarciajs.js
@@ -28,52 +28,20 @@ $(document).ready(function() {
 	$("#uxui-tag").click(function () {
 		$("#uxui-tag").addClass("dev-tag-selected");
 		$("#dev-tag").removeClass("dev-tag-selected");
-		$("#editorial-tag").removeClass("dev-tag-selected");
-		$("#brand-tag").removeClass("dev-tag-selected");
-		$(".editorial-item").hide();
-		$(".brand-item").hide();
 		$(".dev-item").hide();
 		$(".uxui-item").show();
 	});
 	$("#dev-tag").click(function () {
 		$("#dev-tag").addClass("dev-tag-selected");
-		$("#editorial-tag").removeClass("dev-tag-selected");
-		$("#brand-tag").removeClass("dev-tag-selected");
 		$("#uxui-tag").removeClass("dev-tag-selected");
-		$(".editorial-item").hide();
-		$(".brand-item").hide();
 		$(".uxui-item").hide();
 		$(".dev-item").show();
-	});
-	$("#editorial-tag").click(function () {
-		$("#editorial-tag").addClass("dev-tag-selected");
-		$("#dev-tag").removeClass("dev-tag-selected");
-		$("#brand-tag").removeClass("dev-tag-selected");
-		$("#uxui-tag").removeClass("dev-tag-selected");
-		$(".dev-item").hide();
-		$(".brand-item").hide();
-		$(".uxui-item").hide();
-		$(".editorial-item").show();
-	});
-	$("#brand-tag").click(function () {
-		$("#brand-tag").addClass("dev-tag-selected");
-		$("#dev-tag").removeClass("dev-tag-selected");
-		$("#editorial-tag").removeClass("dev-tag-selected");
-		$("#uxui-tag").removeClass("dev-tag-selected");
-		$(".dev-item").hide();
-		$(".editorial-item").hide();
-		$(".uxui-item").hide();
-		$(".brand-item").show();
 	});
 	$("#all-tag").click(function () {
 		$("#uxui-tag").removeClass("dev-tag-selected");
 		$("#dev-tag").removeClass("dev-tag-selected");
-		$("#editorial-tag").removeClass("dev-tag-selected");
-		$("#brand-tag").removeClass("dev-tag-selected");
 		$(".uxui-item").show();
 		$(".dev-item").show();
-		$(".editorial-item").show();
-		$(".brand-item").show();
 	});
 });
 
